33e436fde9bf4359fd8f4d36eb18784d52a48f38
Zuo Xiang <xianglinks@gmail.com>
2022-07-22 18:49:20 +0200

    Add more tests

---
+++ algorithm_data_structure/trie/README.md
@@ -0,0 +1,3 @@
+# Trie
+
+This directory contains implementation of the trie data structure (also called as digital tree or prefix tree).
---
+++ algorithm_data_structure/trie/trie.c
@@ -0,0 +1,17 @@
+/*
+ * trie.c
+ * Copyright (C) 2022 zuo <zuo@zuo-alienware15r3>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	printf("Main function exists.\n");
+	return EXIT_SUCCESS;
+}
