b97ba5e901c4f192bfd5713a0e4f52e7c364d87a
Zuo Xiang <xianglinks@gmail.com>
2023-03-21 23:04:09 +0800

    Finish chapter 03 of LDDD2

---
+++ linux/lddd2/Chapter03/cmwq.c
@@ -0,0 +1,45 @@
+#define pr_fmt(fmt) "PACKT-03-cmwq: " fmt
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/slab.h>		 /* for kmalloc() */
+#include <linux/workqueue.h> /* for work queue */
+
+static struct workqueue_struct *wq;
+
+struct work_data {
+	struct work_struct my_work;
+	int the_data;
+};
+
+static void work_handler(struct work_struct *work) {
+	struct work_data *my_data = container_of(work, struct work_data, my_work);
+	pr_info("CMWQ module handler: %s, data is %d\n", __func__,
+			my_data->the_data);
+	kfree(my_data);
+}
+
+static int __init my_init(void) {
+	struct work_data *my_data;
+
+	pr_info("CMWQ  module init: %s %d\n", __func__, __LINE__);
+	wq = alloc_workqueue("cmwp-example",
+						 /* we want it to be unbound and high priority */
+						 WQ_UNBOUND | WQ_HIGHPRI, 0);
+	my_data = kmalloc(sizeof(struct work_data), GFP_KERNEL);
+	my_data->the_data = 17;
+
+	INIT_WORK(&my_data->my_work, work_handler);
+	queue_work(wq, &my_data->my_work);
+	return 0;
+}
+
+static void __exit my_exit(void) {
+	flush_workqueue(wq);
+	destroy_workqueue(wq);
+	pr_info("Work queue module exit: %s %d\n", __func__, __LINE__);
+}
+
+module_init(my_init);
+module_exit(my_exit);
+MODULE_LICENSE("GPL");
