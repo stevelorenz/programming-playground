e7a26a4f2a3eab50c50db01589bdc368b56cf26f
Zuo Xiang <zuoxiang@cisco.com>
2025-10-10 13:44:53 +0800

    Tmp commit

---
+++ linux/sysfs/hello_sysfs.c
@@ -0,0 +1,54 @@
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kobject.h>
+#include <linux/module.h>
+#include <linux/string.h>
+#include <linux/sysfs.h>
+
+static struct kobject *mymodule;
+
+static int myvariable = 0;
+
+static ssize_t myvariable_show(struct kobject *kobj,
+                               struct kobj_attribute *attr, char *buf) {
+    return sprintf(buf, "%d\n", myvariable);
+}
+
+static ssize_t myvariable_store(struct kobject *kobj,
+                                struct kobj_attribute *attr, const char *buf,
+                                size_t count) {
+    sscanf(buf, "%d", &myvariable);
+    return count;
+}
+
+static struct kobj_attribute myvariable_attribute =
+    __ATTR(myvariable, 0660, myvariable_show, myvariable_store);
+
+static int __init mymodule_init(void) {
+    int error = 0;
+
+    pr_info("mymodule: initialized\n");
+
+    mymodule = kobject_create_and_add("mymodule", kernel_kobj);
+    if (!mymodule)
+        return -ENOMEM;
+
+    error = sysfs_create_file(mymodule, &myvariable_attribute.attr);
+    if (error) {
+        kobject_put(mymodule);
+        pr_info("failed to create the myvariable file "
+                "in /sys/kernel/mymodule\n");
+    }
+
+    return error;
+}
+
+static void __exit mymodule_exit(void) {
+    pr_info("mymodule: Exit success\n");
+    kobject_put(mymodule);
+}
+
+module_init(mymodule_init);
+module_exit(mymodule_exit);
+
+MODULE_LICENSE("GPL");
